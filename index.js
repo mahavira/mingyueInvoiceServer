416b3d4aee42c20211bd4532de452f43d6f3e71e
linmingxiong <272921246@qq.com>
2017-07-06 18:52:30 +0800

    ~

--- index.js
+++ index.js
@@ -5,23 +5,28 @@ var app = express()
 app.use(bodyParser.urlencoded({
 	extended: false
 }))
+//allow custom header and CORS
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
 
-app.all('*', function(req, res, next) {
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
-	res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-	res.header("Content-Type", "application/json;charset=utf-8");
-	next()
-})
-app.get('/', function(req, res) {
+  if (req.method == 'OPTIONS') {
+    res.sendStatus(200).end()
+  }
+  else {
+    next();
+  }
+});
+app.get('/', function (req, res) {
 	res.send('Hello World')
 })
-app.use('/project_dzff/app/smsCode', function(req, res) {
+app.use('/project_dzff/app/smsCode', function (req, res) {
 	var result = {
 		res_code: 400,
 		res_data: ""
 	}
-	if(req.body.smsCode === '1234') {
+	if (req.body.smsCode === '1234') {
 		result = {
 			"res_code": 200,
 			"res_data": "成功"
@@ -33,7 +38,7 @@ app.use('/project_dzff/app/smsCode', function(req, res) {
 /**
  * login
  */
-app.post('/project_dzff/app/login/login', function(req, res) {
+app.post('/project_dzff/app/login/login', function (req, res) {
 	res.json({
 		"res_data": {
 			"id": "1",
@@ -64,7 +69,7 @@ app.post('/project_dzff/app/login/login', function(req, res) {
 /**
  * register
  */
-app.post('/project_dzff/app/login/register', function(req, res) {
+app.post('/project_dzff/app/login/register', function (req, res) {
 	res.json({
 		"res_data": {
 			"id": "6d6bd1d5ee7b473caec52be80f589130",
@@ -88,7 +93,7 @@ app.post('/project_dzff/app/login/register', function(req, res) {
 /**
  * updateEmail
  */
-app.post('/project_dzff/app/user/updateEmail', function(req, res) {
+app.post('/project_dzff/app/user/updateEmail', function (req, res) {
 	res.json({
 		"res_data": "成功",
 		"res_code": 200
@@ -97,7 +102,7 @@ app.post('/project_dzff/app/user/updateEmail', function(req, res) {
 /**
  * updateUserName
  */
-app.post('/project_dzff/app/user/updateUserName', function(req, res) {
+app.post('/project_dzff/app/user/updateUserName', function (req, res) {
 	res.json({
 		"res_data": "成功",
 		"res_code": 200
@@ -106,7 +111,7 @@ app.post('/project_dzff/app/user/updateUserName', function(req, res) {
 /**
  * updateUserFinance
  */
-app.post('/project_dzff/app/user/updateUserFinance', function(req, res) {
+app.post('/project_dzff/app/user/updateUserFinance', function (req, res) {
 	res.json({
 		"res_data": "成功",
 		"res_code": 200
@@ -115,7 +120,7 @@ app.post('/project_dzff/app/user/updateUserFinance', function(req, res) {
 /**
  * updateUserPassword
  */
-app.post('/project_dzff/app/user/updateUserPassword', function(req, res) {
+app.post('/project_dzff/app/user/updateUserPassword', function (req, res) {
 	res.json({
 		"res_data": "成功",
 		"res_code": 200
@@ -124,78 +129,79 @@ app.post('/project_dzff/app/user/updateUserPassword', function(req, res) {
 /**
  * getMyMessages
  */
-app.post('/project_dzff/app/message/getMyMessages', function(req, res) {
+app.get('/project_dzff/app/message/getMyMessages', function (req, res) {
 	res.json({
 		"res_data": {
 			"pageNo": 1,
 			"pageSize": 30,
 			"count": 5,
 			"list": [{
-					"id": "1",
-					"isNewRecord": false,
-					"remarks": "备注",
-					"createDate": "2017-06-18 10:16:28",
-					"updateDate": "2017-06-18 10:16:31",
-					"title": "您的发票已经打印",
-					"content": "您的发票已经打印",
-					"toUser": "1",
-					"isRead": "0"
-				},
-				{
-					"id": "2",
-					"isNewRecord": false,
-					"remarks": "备注",
-					"createDate": "2017-06-18 10:16:28",
-					"updateDate": "2017-06-18 10:16:31",
-					"title": "您的发票已经打印",
-					"content": "您的发票已经打印",
-					"toUser": "1",
-					"isRead": "0"
-				},
-				{
-					"id": "3",
-					"isNewRecord": false,
-					"remarks": "备注",
-					"createDate": "2017-06-18 10:16:28",
-					"updateDate": "2017-06-18 10:16:31",
-					"title": "您的发票已经打印",
-					"content": "您的发票已经打印",
-					"toUser": "1",
-					"isRead": "0"
-				},
-				{
-					"id": "4",
-					"isNewRecord": false,
-					"remarks": "备注",
-					"createDate": "2017-06-18 10:16:28",
-					"updateDate": "2017-06-18 10:16:31",
-					"title": "您的发票已经打印",
-					"content": "您的发票已经打印",
-					"toUser": "1",
-					"isRead": "0"
-				},
-				{
-					"id": "5",
-					"isNewRecord": false,
-					"remarks": "备注",
-					"createDate": "2017-06-18 10:16:28",
-					"updateDate": "2017-06-18 10:16:31",
-					"title": "您的发票已经打印",
-					"content": "您的发票已经打印",
-					"toUser": "1",
-					"isRead": "0"
-				}
+				"id": "1",
+				"isNewRecord": false,
+				"remarks": "备注",
+				"createDate": "2017-06-18 10:16:28",
+				"updateDate": "2017-06-18 10:16:31",
+				"title": "您的发票已经打印",
+				"content": "您的发票已经打印",
+				"toUser": "1",
+				"isRead": "0"
+			},
+			{
+				"id": "2",
+				"isNewRecord": false,
+				"remarks": "备注",
+				"createDate": "2017-06-18 10:16:28",
+				"updateDate": "2017-06-18 10:16:31",
+				"title": "您的发票已经打印",
+				"content": "您的发票已经打印",
+				"toUser": "1",
+				"isRead": "0"
+			},
+			{
+				"id": "3",
+				"isNewRecord": false,
+				"remarks": "备注",
+				"createDate": "2017-06-18 10:16:28",
+				"updateDate": "2017-06-18 10:16:31",
+				"title": "您的发票已经打印",
+				"content": "您的发票已经打印",
+				"toUser": "1",
+				"isRead": "0"
+			},
+			{
+				"id": "4",
+				"isNewRecord": false,
+				"remarks": "备注",
+				"createDate": "2017-06-18 10:16:28",
+				"updateDate": "2017-06-18 10:16:31",
+				"title": "您的发票已经打印",
+				"content": "您的发票已经打印",
+				"toUser": "1",
+				"isRead": "0"
+			},
+			{
+				"id": "5",
+				"isNewRecord": false,
+				"remarks": "备注",
+				"createDate": "2017-06-18 10:16:28",
+				"updateDate": "2017-06-18 10:16:31",
+				"title": "您的发票已经打印",
+				"content": "您的发票已经打印",
+				"toUser": "1",
+				"isRead": "0"
+			}
 			],
 			"firstResult": 0,
 			"maxResults": 30
 		},
 		"res_code": 200
 	})
+	res.end()
 })
 /**
  * getFinanceUsers
  */
-app.post('/project_dzff/app/login/getFinanceUsers', function(req, res) {
+app.post('/project_dzff/app/login/getFinanceUsers', function (req, res) {
 	res.json({
 		"res_data": [{
 			"id": "1",
@@ -211,7 +217,7 @@ app.post('/project_dzff/app/login/getFinanceUsers', function(req, res) {
 /**
  * getUserByMobile
  */
-app.post('/project_dzff/app/login/getUserByMobile', function(req, res) {
+app.post('/project_dzff/app/login/getUserByMobile', function (req, res) {
 	res.json({
 		"res_data": "0", // 0:表示可用 1:表示已经被占用
 		"res_code": 200
@@ -220,7 +226,7 @@ app.post('/project_dzff/app/login/getUserByMobile', function(req, res) {
 /**
  * getUserByEmail
  */
-app.post('/project_dzff/app/login/getUserByEmail', function(req, res) {
+app.post('/project_dzff/app/login/getUserByEmail', function (req, res) {
 	res.json({
 		"res_data": "0", // 0:表示可用 1:表示已经被占用
 		"res_code": 200
@@ -229,7 +235,7 @@ app.post('/project_dzff/app/login/getUserByEmail', function(req, res) {
 /**
  * uploadUrl
  */
-app.post('/project_dzff/app/bill/uploadUrl', function(req, res) {
+app.post('/project_dzff/app/bill/uploadUrl', function (req, res) {
 	res.json({
 		"res_data": "成功",
 		"res_code": 200
@@ -238,7 +244,7 @@ app.post('/project_dzff/app/bill/uploadUrl', function(req, res) {
 /**
  * getUserByEmail
  */
-app.post('/project_dzff/app/bill/uploadPdf', function(req, res) {
+app.post('/project_dzff/app/bill/uploadPdf', function (req, res) {
 	res.json({
 		"res_data": "成功",
 		"res_code": 200
@@ -247,20 +253,20 @@ app.post('/project_dzff/app/bill/uploadPdf', function(req, res) {
 /**
  * invoice
  */
-app.get('/invoice', function(req, res) {
+app.get('/invoice', function (req, res) {
 	var data = []
 	var maxId = 37
 	var minId = 3
 	var id = parseInt(req.query.id) + 1 || minId
 	var type = req.query.type || 'min'
-	if(type === 'min') {
-		for(let i = id; i < id + 10 && i <= maxId; i++) {
+	if (type === 'min') {
+		for (let i = id; i < id + 10 && i <= maxId; i++) {
 			data.push(getItem(i))
 		}
 	} else {
 		let i = id - 11;
 		i = i < minId ? minId : i
-		for(; i < id - 1; i++) {
+		for (; i < id - 1; i++) {
 			data.push(getItem(i))
 		}
 	}
@@ -280,7 +286,7 @@ app.get('/invoice', function(req, res) {
 		}
 	}
 })
-app.post('/invoice', function(req, res) {
+app.post('/invoice', function (req, res) {
 	res.json({
 		code: 200,
 		message: '正确'
